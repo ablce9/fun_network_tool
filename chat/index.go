6d4b8cc10c432bc304e258992b7d053ddf4d31fc
Shunsuke Tamiya <shunsuketamiya@posteo.net>
2018-11-30 17:24:51 +0900

    An improvement toward mobile
    That is, editing html for mobile screens specifically safari and
    add some styling also for mobile.

--- chat/index.go
+++ chat/index.go
@@ -4,81 +4,94 @@ import "fmt"
 
 // App is like jsx.
 func App(wsserver string) string {
-	return fmt.Sprintf(`<html>
+        return fmt.Sprintf(`<html>
     <head>
-	<meta charset="utf-8">
-	<style type="text/css">
-	 #container {
-	     position: relative;
-	     font-family: helvetica;
-	 }
-	 #bottom {
-	     position: fixed;
-	     bottom: 0px;
-	 }
-	 #messages {
-	     padding: 0em 0em;
-	 }
-	 ul, li {
-	     list-style: none;
-	 }
-	 input {
-	     display: block;
-	     margin-bottom: 10px;
-	     padding: 5px;
-	     font-size: 16px;
-	     width: 500px;
-	     max-width: 800px;
-	     min-width: 400px;
-	 }
-	</style>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1">
+        <style type="text/css">
+         body {
+             width: 100%%;
+             font-family: sans-serif;
+             margin: 0;
+             padding: 0;
+         }
+         @media screen and (.webkit-min-device-pixel-ration:0) {
+             select,
+             textarea,
+             input {
+                 front-size: 20px;
+             }
+         }
+         #container {
+             position: relative;
+             font-family: helvetica;
+         }
+         #bottom {
+             position: fixed;
+             bottom: 0px;
+         }
+         #messages {
+             padding: 0em 0em;
+         }
+         ul, li {
+             list-style: none;
+         }
+         input {
+             display: block;
+             margin-bottom: 10px;
+             padding: 5px;
+             width: 100%%;
+             max-width: 800px;
+             min-width: 400px;
+         }
+        </style>
     </head>
     <body>
-	<div id="container">
-	    <div id="message-grid">
-		<ul id="messages"></ul>
-	    </div>
-	    <div id="bottom">
-		<input id="input-field" />
-	    </div>
-	</div>
-	<script>
-	 function connect (wsserver, onOpenFunc, onMessageFunc) {
-	     return new Promise(function(resolve, reject) {
-		 const socket = new WebSocket(wsserver);
-		 socket.addEventListener('open', function() {
-		     onOpenFunc(socket);
-		 });
-		 socket.addEventListener('message', function(evt) {
-		     onMessageFunc(evt);
-		 });
-		 socket.onopen = function() {
-		     resolve(socket);
-		 };
-		 socket.onerror = function(err) {
-		     reject(err);
-		 };
-	     });
-	 };
-	 const onOpenFunc = function(sock) {
-	     const input = document.getElementById('input-field');
-	     input.addEventListener('change', function(evt) {
-		 sock.send(evt.target.value);
-		 evt.target.value = '';
-	     });
-	 };
-	 const onMessageFunc = function(evt) {
-	     const grid = document.getElementById('messages')
-	     const data = evt.data;
-	     const timestamp = new Date();
-	     grid.innerHTML += '<li>'+timestamp.toLocaleString()+': '+data+'</li>';
-	 };
-	 connect('ws://%s/chat-room', onOpenFunc, onMessageFunc).then(function(conn) {
-	     console.log('connected:', conn);
-	 }).catch(function(err) {
-	     throw('error:', err);
-	 });
-	</script>
+        <div id="container">
+            <div id="message-grid">
+                <ul id="messages"></ul>
+            </div>
+            <div id="bottom">
+                <input id="input-field" />
+            </div>
+        </div>
+        <script>
+         function connect (wsserver, onOpenFunc, onMessageFunc) {
+             return new Promise(function(resolve, reject) {
+                 const socket = new WebSocket(wsserver);
+                 socket.addEventListener('open', function() {
+                     onOpenFunc(socket);
+                 });
+                 socket.addEventListener('message', function(evt) {
+                     onMessageFunc(evt);
+                 });
+                 socket.onopen = function() {
+                     resolve(socket);
+                 };
+                 socket.onerror = function(err) {
+                     reject(err);
+                 };
+             });
+         };
+         const onOpenFunc = function(sock) {
+             const input = document.getElementById('input-field');
+             input.addEventListener('change', function(evt) {
+                 sock.send(evt.target.value);
+                 evt.target.value = '';
+             });
+         };
+         const onMessageFunc = function(evt) {
+             const grid = document.getElementById('messages')
+             const data = evt.data;
+             const timestamp = new Date();
+             grid.innerHTML += '<li>'+timestamp.toLocaleString()+': '+data+'</li>';
+         };
+         connect('ws://%s/chat-room', onOpenFunc, onMessageFunc).then(function(conn) {
+             console.log('connected:', conn);
+         }).catch(function(err) {
+             throw('error:', err);
+         });
+        </script>
     </body>
 </html>
 `, wsserver)
